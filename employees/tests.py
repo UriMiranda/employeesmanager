56c765e2e59194382f9df1171e55ce801b677d84
Uriel Miranda <miranda.uriel@gmail.com>
2019-06-22 21:17:22 -0300

    Add create and delete test

--- employees/tests.py
+++ employees/tests.py
@@ -2,14 +2,38 @@ import unittest
 from django.test import TestCase, Client
 from .models import Employee, Departament
 import datetime
-
-# Create your tests here.
-
+from django.core.exceptions import ValidationError
+import json
 
 class EmployeeTest(unittest.TestCase):
     def setUp(self):
         self.client = Client()
+        self.departament = Departament.objects.create(name='T.I')
 
-    def test_employee_list(self):
-        response = self.client.get('/employees')
+    def test_employee_existing_validation(self):
+        try:
+            Employee.objects.create(
+                name='Valid', email='validate@gmail.com', departament=self.departament)
+            Employee.objects.create(
+                name='Valid', email='validate@gmail.com', departament=self.departament)
+            self.assertTrue(False)
+        except (ValidationError):
+            self.assertTrue(True)
+
+    def test_employee_create_api(self):
+        datetime_hash = datetime.datetime.now().strftime('%d-%m-%Y-%H-%M-%S')
+        response = self.client.post('/employees/store', {
+            'name': 'teste-'+datetime_hash,
+            'email': 'test-'+datetime_hash+'@gmail.com',
+            'departament': '1'
+        }, content_type="application/json", format="json")
         self.assertEqual(response.status_code, 200)
+
+    def test_employee_list_api(self):
+        response = self.client.get(
+            '/employees', HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(response.status_code, 301)
+
+    def test_departament_delete_api(self):
+        response = self.client.get('/employees/delete/2',HTTP_X_REQUESTED_WITH='XMLHttpRequest')
+        self.assertEqual(response.status_code, 301)
